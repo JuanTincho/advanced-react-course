a3b07ccbe4ab0620fb5e01a7cde7e01ee1a64029
Juan Martín Pithod <juanmartin.pithod@intive.com>
2020-07-12 19:48:09 -0300

    Styled components module

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,24 @@
 import Link from 'next/link';
+import NavStyled from './styles/NavStyles';
 
 const Nav = (props) => (
-  <div>
+  <NavStyled>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>
     <Link href="/sell">
-      <a>Sell!</a>
+      <a>Sell</a>
     </Link>
-    <Link href="/">
-      <a>Home!</a>
+    <Link href="/signup">
+      <a>Signup</a>
     </Link>
-  </div>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyled>
 );
 
 export default Nav;
